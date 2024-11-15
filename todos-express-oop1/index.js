8530409f07f9c0bae6631c321550bd448f7c6fbe
Randel Väär <152606802+shkyn@users.noreply.github.com>
2024-11-15 11:46:11 +0200

    Update index.js

--- todos-express-oop1/index.js
+++ todos-express-oop1/index.js
@@ -1,14 +1,14 @@
-import express from "express"
+import express from 'express'
 import bodyParser from "body-parser"
+
+import todoRoutes from './routes/todos.js'
 
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/json-test', (req,res) => {
-    res.send({
-        message: "json test ok"
-    })
-})
+app.use(express.urlencoded({extended: true}))
+
+app.use('/todos', todoRoutes)
 
 app.listen(3009, () => {
     console.log("server is connected to port 3009")
