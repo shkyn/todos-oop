2e12ca54f36387bbb86880f1c99c84291f195d5b
Randel Väär <152606802+shkyn@users.noreply.github.com>
2024-11-19 11:48:09 +0200

    Update todos.js
    delete, async

--- todos-express-oop1/controllers/todos.js
+++ todos-express-oop1/controllers/todos.js
@@ -1,24 +1,61 @@
-import { Todo } from "../models/todo.js"
+import { Todo } from "../models/todo.js";
 
 class todoController {
-    constructor(){
+    constructor() {
         this.TODOS = []
     }
 
-    createTodo(req, res){
-        const task = req.body.task
-        const newTodo = new Todo(Math.random().toString(), task)
-        this.TODOS.push(newTodo)
+
+    async createTodo(req, res) {
+        const task = req.body.task;
+        const newTodo = new Todo(Math.random().toString(), task);
+        this.TODOS.push(newTodo);
+        await fileManager.writeFile('./data/todos.json', this.TODOS)
         res.json({
-            message: "created new todo object",
+            message: "Created new todo object",
             newTask: newTodo
-        })
+        });
     }
-    getTodos(req, res){
-        res.json({tasks: this.TODOS})
+
+    getTodos(req, res) {
+        res.json({tasks: this.TODOS});
     }
+
+    updateTodo(req, res) {
+        const todoId = req.params.id;
+        const updatedTask = req.body.task;
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId);
+
+        if (todoIndex < 0) {
+            throw new Error("Could not find todo!")
+            res.json({
+                message: "Could not find todo with such index"
+            })
+        }
+
+        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask);
+        res.json({
+            message: "Updated todo",
+            updatedTask: this.TODOS[todoIndex]
+        });
+    }
+    
+    deleteTodo(req, res) {
+        const todoId = req.params.id;
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId);
+
+        if (todoIndex < 0) {
+            return res.status(404).json({
+                message: "Could not find todo with the given id",
+            });
+        }
+
+        this.TODOS.splice(todoIndex, 1); // Eemalda element massiivist
+        res.json({
+            message: "Deleted todo successfully",
+        });
+    }
+
 }
 
-
-
-export const TodoController = new todoController()
+export const TodoController = new todoController();
